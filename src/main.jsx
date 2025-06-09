e2cc8dad1b9c2a970176fc1c6da135d4516d866c
sigmaTUFF <jakobm248@gmail.com>
2025-06-09 19:04:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import MainMenu from "./MainMenu";  // Hier importierst du deine Hauptkomponente
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MainMenu />  {/* Deine Hauptkomponente */}
   </React.StrictMode>
 );
