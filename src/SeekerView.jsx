6e2623d8ca62b074bd83cc4d2b05461c70681b76
sigmaTUFF <jakobm248@gmail.com>
2025-06-09 18:53:24 +0200

    Delete src/SeekerView.jsx

--- src/SeekerView.jsx
+++
@@ -1,8 +0,0 @@
-export default function SeekerView() {
-  return (
-    <div className="text-center p-4">
-      <h1 className="text-xl font-bold">Seeker Ansicht</h1>
-      <p>Hier kannst du später dein Seeker-Feature implementieren.</p>
-    </div>
-  );
-}
